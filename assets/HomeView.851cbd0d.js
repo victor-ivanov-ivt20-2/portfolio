1474060554e7784835c115507937f06f1b350333
victor-ivanov-ivt20-2 <72002526+victor-ivanov-ivt20-2@users.noreply.github.com>
2022-08-03 21:43:14 +0900

    deploy

---
+++ assets/HomeView.851cbd0d.js
@@ -0,0 +1,1 @@
+import{d as e,_ as o,o as s,c as t}from"./index.a3d3dd65.js";const c=e({}),n={class:"home"};function a(r,_,p,d,i,f){return s(),t("div",n," hello ")}const m=o(c,[["render",a]]);export{m as default};
